ddc0275ddf66b43f38b199be45f82dcac959e5e5
stagebo <1254117589@qq.com>
2017-07-14 23:58:40 +0800

    good night

--- src/com/dyi/leetcode/Test.java
+++ src/com/dyi/leetcode/Test.java
@@ -3,18 +3,22 @@ package com.dyi.leetcode;
 public class Test {
 	public static void main(String[] args) {
 		String s="11+1*33+3/2";
-		s="01+1";
+		s="021+1-9/2";
 		System.out.println(calculate(s));
 	}
 	public static int calculate(String s) {
         s=s.replace(" ","");
         String[] sNum=s.split("[+-/*]");
         String[] cNum=s.split("[0123456789]+");
-        
+        if(cNum.length>1){
+        	String[] temp=new String[cNum.length-1];
+        	for(int i=0;i<temp.length;i++)temp[i]=cNum[i+1];
+        	cNum=temp;
+        }
         String[] str=new String[sNum.length+cNum.length];
         int index=0;
         for(int i=0;i<cNum.length;i++){
-        	str[index++]=sNum[i-1];
+        	str[index++]=sNum[i];
         	str[index++]=cNum[i];
         	
         }
@@ -24,11 +28,15 @@ public class Test {
         for(int i=0;i<str.length;i++){
             if("*".equals(str[i])){
                 int a=Integer.parseInt(str[i-1])*Integer.parseInt(str[i+1]);
-                str[i-1]=0+"";str[i]="+";str[i+1]=a+"";
+                str[i-1]=0+"";
+                str[i]=i>2?str[i-2]:"+";
+                str[i+1]=a+"";
                 
             }else if("/".equals(str[i])){
                 int a=Integer.parseInt(str[i-1])/Integer.parseInt(str[i+1]);
-                str[i-1]=0+"";str[i]="+";str[i+1]=a+"";
+                str[i-1]=0+"";
+                str[i]=i>2?str[i-2]:"+";
+                str[i+1]=a+"";
             }
             
         }
