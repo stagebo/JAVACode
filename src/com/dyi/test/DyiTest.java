519608cbc6cc2e0310dc2c3bd34c0cbc26c90c31
stagebo <1254117589@qq.com>
2017-08-01 10:53:59 +0800

    2017年8月1日10:53:57

--- src/com/dyi/test/DyiTest.java
+++ src/com/dyi/test/DyiTest.java
@@ -2,6 +2,7 @@ package com.dyi.test;
 
 public class DyiTest {
 	public static void main(String[] args) {
-		System.out.println('a');
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.toBinaryString(21474778));
 	}
 }
