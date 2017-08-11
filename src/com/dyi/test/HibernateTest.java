ed146a5ae012a670ec32ca5045653b3b3647f27b
stagebo <1254117589@qq.com>
2017-08-11 17:02:23 +0800

    添加sessionmanager class.2017-8-11 17:02:21

--- src/com/dyi/test/HibernateTest.java
+++ src/com/dyi/test/HibernateTest.java
@@ -12,34 +12,41 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.NativeQuery;
 
+import com.dyi.hibernate.SessionManager;
 import com.dyi.hibernate.model.User;
 
 
 public class HibernateTest {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-		Configuration configuration = new Configuration();
-		configuration.configure("/hibernate.cfg.xml");
-		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		
 
-		Session session = sessionFactory.openSession();
+		Session session = SessionManager.openSession();
 		Transaction tran=session.beginTransaction();
 		User u = new User();
 		u.setF_id(UUID.randomUUID().toString());
 		u.setF_uid("sts");
 		u.setF_pwd("sts");
-		u.setF_reg_date(new Date());
+		u.setF_reg_date(new Date("2017-8-11"));
 		u.setF_exist(1);
 
 		session.save(u);
 		tran.commit();
 
 		NativeQuery q=session.createSQLQuery("select * from t_user");
-		
 		List<Object[]> ul=q.list();
+
+		tran=session.beginTransaction();
+		session.delete(u);
+		tran.commit();
+		
+		System.out.println(ul.size());
 		for(Object[] o:ul){
 			System.out.println(Arrays.toString(o));
 		}
+		
+		
+		
 		session.close();
 		
 		
