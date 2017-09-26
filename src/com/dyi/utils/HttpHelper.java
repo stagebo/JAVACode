17850ed43f08aecf91e4925e7e495296a038108d
stagebo <1254117589@qq.com>
2017-09-26 14:03:12 +0800

    2017-9-26 14:03:10

--- src/com/dyi/utils/HttpHelper.java
+++ src/com/dyi/utils/HttpHelper.java
@@ -5,6 +5,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import net.sf.json.JSONObject;
@@ -25,17 +26,41 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
-
 public class HttpHelper {
 	private CloseableHttpClient client;
 
 	public HttpHelper() {
-		this.client = HttpClients.createDefault();
+		this.client = HttpClients.createDefault();		
 	}
 
 	public String sendGet(String url) throws ParseException, IOException {
 		String respContent = "";
 		HttpGet httpPost = new HttpGet(url);
+		
+		HttpResponse resp = client.execute(httpPost);
+		if (resp.getStatusLine().getStatusCode() == 200) {
+			HttpEntity he = resp.getEntity();
+			respContent = EntityUtils.toString(he, "UTF-8");
+		}
+		return respContent;
+	}
+
+	/**
+	 * 
+	 * @param url
+	 * @param pairList
+	 * @return
+	 * @throws ParseException
+	 * @throws IOException
+	 */
+	public String sendPost(String url, List<BasicNameValuePair> pairList)
+			throws ParseException, IOException {
+		String respContent = "";
+		HttpPost httpPost = new HttpPost(url);
+
+		// 表单方式
+
+		httpPost.setEntity(new UrlEncodedFormEntity(pairList, "utf-8"));
 
 		HttpResponse resp = client.execute(httpPost);
 		if (resp.getStatusLine().getStatusCode() == 200) {
@@ -45,15 +70,49 @@ public class HttpHelper {
 		return respContent;
 	}
 
-	public String sendPost(String url, List<BasicNameValuePair> pairList) throws ParseException,
-			IOException {
+	public String sendPost(String url, Map<String, String> paramMap)
+			throws ParseException, IOException {
 		String respContent = "";
 		HttpPost httpPost = new HttpPost(url);
 
 		// 表单方式
-		 
-		 httpPost.setEntity(new UrlEncodedFormEntity(pairList, "utf-8"));
+		if (paramMap != null) {
 
+			List<BasicNameValuePair> paramList = new ArrayList<BasicNameValuePair>();
+			for(String key:paramMap.keySet()){
+				paramList.add(new BasicNameValuePair(key, paramMap.get(key)));
+			}
+			httpPost.setEntity(new UrlEncodedFormEntity(paramList, "utf-8"));
+		}
+
+		HttpResponse resp = client.execute(httpPost);
+		if (resp.getStatusLine().getStatusCode() == 200) {
+			HttpEntity he = resp.getEntity();
+			respContent = EntityUtils.toString(he, "UTF-8");
+		}
+		return respContent;
+	}
+	
+	public String sendPost(String url, Map<String, String> paramMap,Map<String,String> headerMap)
+			throws ParseException, IOException {
+		String respContent = "";
+		HttpPost httpPost = new HttpPost(url);
+
+		// 表单方式
+		if (paramMap != null) {
+
+			List<BasicNameValuePair> paramList = new ArrayList<BasicNameValuePair>();
+			for(String key:paramMap.keySet()){
+				paramList.add(new BasicNameValuePair(key, paramMap.get(key)));
+			}
+			httpPost.setEntity(new UrlEncodedFormEntity(paramList, "utf-8"));
+		}
+		//添加请求头
+		if(headerMap != null){
+			for(String key : headerMap.keySet()){
+				httpPost.addHeader(key, headerMap.get(key));
+			}			
+		}
 		HttpResponse resp = client.execute(httpPost);
 		if (resp.getStatusLine().getStatusCode() == 200) {
 			HttpEntity he = resp.getEntity();
